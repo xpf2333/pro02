c89f3e470f28623ccb44544b3956419dc38484d0
xpf <1289523207@qq.com>
2022-08-27 10:55:30 +0800

    test

--- pro02ch/src/main/java/com/atguigu/maven/HbaseConnection.java
+++ pro02ch/src/main/java/com/atguigu/maven/HbaseConnection.java
@@ -2,6 +2,7 @@ package com.atguigu.maven;
 
 public class HbaseConnection {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("aiwo");
+        System.out.println("aini");
     }
 }
