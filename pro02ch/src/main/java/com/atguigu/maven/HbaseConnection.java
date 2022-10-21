3e191457cd178195cedb63d7168bf7144a65b155
xpf2333 <35884311+xpf2333@users.noreply.github.com>
2022-10-21 23:12:47 +0800

    Update HbaseConnection.java
    test

--- pro02ch/src/main/java/com/atguigu/maven/HbaseConnection.java
+++ pro02ch/src/main/java/com/atguigu/maven/HbaseConnection.java
@@ -2,9 +2,9 @@ package com.atguigu.maven;
 
 public class HbaseConnection {
     public static void main(String[] args) {
-        System.out.println("aiwo");
-        System.out.println("aipni");
-        System.out.println("aini");
+        System.out.println("aiw");
+        System.out.println("aii");
+        System.out.println("ai");
     }
 }
 
