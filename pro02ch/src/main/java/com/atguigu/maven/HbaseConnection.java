2153c38929382a99a965c53c9d607ca9e88a1e48
xpf <1289523207@qq.com>
2022-10-21 23:00:52 +0800

    test1

--- pro02ch/src/main/java/com/atguigu/maven/HbaseConnection.java
+++ pro02ch/src/main/java/com/atguigu/maven/HbaseConnection.java
@@ -4,7 +4,7 @@ public class HbaseConnection {
     public static void main(String[] args) {
         System.out.println("aiwo");
         System.out.println("aipni");
-
+        System.out.println("aini");
     }
 }
 
